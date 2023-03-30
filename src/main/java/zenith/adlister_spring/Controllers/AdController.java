2d9bcd18ba2fb444aed0d3203c499677183a2b3b
RydCri <107228115+RydCri@users.noreply.github.com>
2023-03-30 11:07:11 -0700

    added TagRepository
    tagsDao to AdController constructor
    
    added create tag logic to adsPost method
    - inc logic to prevent duplicated tags from posting to database

--- src/main/java/zenith/adlister_spring/Controllers/AdController.java
+++ src/main/java/zenith/adlister_spring/Controllers/AdController.java
@@ -6,24 +6,32 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import zenith.adlister_spring.models.Ad;
+import zenith.adlister_spring.models.Tag;
+import zenith.adlister_spring.models.Tags;
 import zenith.adlister_spring.repositories.AdRepository;
+import zenith.adlister_spring.repositories.TagRepository;
 import zenith.adlister_spring.repositories.UserRepository;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @Controller
 public class AdController {
     private final AdRepository adsDao;
     private final UserRepository usersDao;
-    public AdController(AdRepository adsDao, UserRepository usersDao)
+    private final TagRepository tagsDao;
+    public AdController(AdRepository adsDao, UserRepository usersDao, TagRepository tagsDao)
     {
         this.usersDao = usersDao;
         this.adsDao = adsDao;
+        this.tagsDao = tagsDao;
     }
 
     @GetMapping("/ads")
     public String adsIndex(Model model) {
         List<Ad> ads = adsDao.findAll();
+        System.out.println(ads);
         model.addAttribute("Modelads", ads);
         return "ads/index";
     }
@@ -33,8 +41,23 @@ public class AdController {
     }
 
     @PostMapping("/ads/post")
-    public String adsPost(@RequestParam String title, @RequestParam String description) {
+    public String adsPost(@RequestParam String title, @RequestParam String description,@RequestParam(name="tags")String tags) {
+        Set<Tag> tagset = Tags.makeTagSet(tags);
         Ad newAd = new Ad(title, description);
+        if (tagset.size() > 0){
+            List<Tag> tagsToAdd = new ArrayList<>();
+            for(Tag tag : tagset){
+                Tag tagFromDb = tagsDao.findTagByName(tag.getName());
+                if(tagFromDb == null){
+                    tagsToAdd.add(tagsDao.save(tag));
+                }else {
+                    tagsToAdd.add(tagFromDb);
+                }
+            }
+            tagset.clear();
+            tagset.addAll(tagsToAdd);
+            newAd.setTags(tagset);
+        }
         adsDao.save(newAd);
 
         return "redirect:/ads";
