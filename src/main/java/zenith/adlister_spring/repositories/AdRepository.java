8ab7423c8107916dcf89ac43fa6104ea983b69b6
RydCri <107228115+RydCri@users.noreply.github.com>
2023-03-30 11:04:14 -0700

    key should've been a long type

--- src/main/java/zenith/adlister_spring/repositories/AdRepository.java
+++ src/main/java/zenith/adlister_spring/repositories/AdRepository.java
@@ -3,7 +3,6 @@ package zenith.adlister_spring.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import zenith.adlister_spring.models.Ad;
 
-import java.math.BigInteger;
+public interface AdRepository extends JpaRepository<Ad, Long> {
 
-public interface AdRepository extends JpaRepository<Ad, BigInteger> {
 }
