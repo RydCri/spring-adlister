ab2d1fc1825fd6de81151d78b6413dbb901cd271
RydCri <107228115+RydCri@users.noreply.github.com>
2023-04-04 10:31:37 -0700

    refactor for new views

--- src/main/java/zenith/adlister_spring/Controllers/UserController.java
+++ src/main/java/zenith/adlister_spring/Controllers/UserController.java
@@ -1,45 +1,36 @@
 package zenith.adlister_spring.Controllers;
 
+
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import zenith.adlister_spring.models.User;
-import zenith.adlister_spring.repositories.AdRepository;
 import zenith.adlister_spring.repositories.UserRepository;
-
-import java.util.List;
 
 @Controller
 public class UserController {
-    private final UserRepository usersDao;
-    public UserController( UserRepository usersDao) {
-        this.usersDao = usersDao;
-    }
-@GetMapping("/register")
-public String getregisterUser() {
-    return "ads/register";
-}
-@GetMapping("/users")
-public String usersIndex(Model model){
-    List<User> users = usersDao.findAll();
-    model.addAttribute("users",users);
-return "ads/userads";
-}
-    @PostMapping("/register")
-    public String registerUser(@RequestParam(name="username")String username,@RequestParam(name="email")String email,@RequestParam(name="password")String password){
-        User user = new User(username,email,password);
-        usersDao.save(user);
-        return "redirect:/ads/userads";
+    private UserRepository userDao;
+    private PasswordEncoder passwordEncoder;
+
+    public UserController(UserRepository userDao, PasswordEncoder passwordEncoder) {
+        this.userDao = userDao;
+        this.passwordEncoder = passwordEncoder;
     }
 
-    @GetMapping("/user/{id}/ads")
-    public String userAds(@PathVariable long id){
-    usersDao.findById(id);
-    return "ads/index";
+    @GetMapping("/sign-up")
+    public String showSignupForm(Model model){
+        model.addAttribute("user", new User());
+        return "users/sign-up";
     }
 
-
+    @PostMapping("/sign-up")
+    public String saveUser(@ModelAttribute User user){
+        String hash = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hash);
+        userDao.save(user);
+        return "redirect:/login";
+    }
 }
